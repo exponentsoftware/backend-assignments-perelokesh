2151482e4ce9286510c3fb609eae490324747bac
perelokesh <lokesh7093@gmail.com>
2022-04-05 16:07:53 +0530

    comment route

---
+++ routes/comment.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/add',);
+router.get('/read');
+router.put('/update',)
+router.delete('/delete');
+
+module.exports = router;
